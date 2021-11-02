448d603b2ba4803efb67885dbe902a2a90e8b753
xXPumbaXx <samuel.drapeau@hotmail.com>
2021-11-02 17:52:38 -0400

    Added state machine

---
+++ Assets/Scripts/State/WizardStateFuite.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardStateFuite : WizardState
+{
+    public override void Init()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ManageStateChange()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void WizardBehavior()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WizardBehavior();
+        ManageStateChange();
+    }
+}
